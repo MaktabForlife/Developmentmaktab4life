cdd879d416b049d219eacc379969fc0d4f95aaf9
GitHub-Maktab4Life <naidu.hajira@gmail.com>
2026-07-15 09:47:17 +0300

    Update worker.js

--- backend/src/worker.js
+++ backend/src/worker.js
@@ -16,7 +16,7 @@ export default {
       return json({
         success: true,
         service: "rebootworker",
-        version: "1.0"
+        version: "2.0"
       });
     }
     if (url.pathname === "/api/resources/list") {
